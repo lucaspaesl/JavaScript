1a8d1fafc132f2e69839431ca2bd14e6b622c0a9
Lucas Lima <plimalucasa@gmail.com>
2024-01-16 17:44:08 -0300

    slsss

---
+++ estudos/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`você estava dirijindo a ${vel}`)
+if (vel > 80) { //condição simples!wd
+    console.log('você ultrapassou a velocidade permitida, VOCÊ FOI MUTADO SEU MERDA!!!')
+}
+console.log('você não esta em um videogame seu merda, toma cuidado com as pessoas seu bosta monte de merda')
---
+++ estudos/aula11/ex009.js
@@ -0,0 +1,8 @@
+var planeta = 'mercurio'
+console.log(`Você mora em ${planeta}`)
+if (planeta == 'Terra') {
+    console.log('Você é um humano de merda, que devera ser aniquilado!!!!!!!!')
+}
+else {
+    console.log('Você é um aliem, tomara que não seja igual a um humano')
+}
