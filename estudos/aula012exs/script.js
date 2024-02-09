cb14bf87ab5e310f943c47ce2416200619c7edfe
LucasLima <plimalucasa@gmail.com>
2024-02-09 20:02:16 -0300

    terminei meu novo projeto

--- estudos/aula012exs/script.js
+++ estudos/aula012exs/script.js
@@ -1,1 +1,21 @@
-window.alert('ola')
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var date = new Date();
+    var hora = date.getHours();
+    msg.innerHTML = `Agora são ${hora} horas`;
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'manha.jpg'; 
+        document.body.style.background = '#adad1a'
+
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = 'tarde.jpg'; 
+        document.body.style.background = '#446767'
+    } else {
+        //bom noite
+        img.src = 'noite.jpg'; 
+        document.body.style.background = '#2e2727'
+    }
+}
