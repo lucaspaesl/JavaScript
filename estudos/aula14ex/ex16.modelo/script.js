96a6452b23470877c54ede42984516f8e2d542b2
LucasLima <plimalucasa@gmail.com>
2024-03-04 18:45:01 -0300

    amém

--- estudos/aula14ex/ex16.modelo/script.js
+++ estudos/aula14ex/ex16.modelo/script.js
@@ -1,21 +1,29 @@
-function carregar() {
-    var msg = window.document.getElementById('msg');
-    var img = window.document.getElementById('imagem');
-    var date = new Date();
-    var hora = date.getHours();
-    msg.innerHTML = `Agora são ${hora} horas`;
-    if (hora >= 0 && hora < 12) {
-        //bom dia
-        img.src = 'manha.jpg'; 
-        document.body.style.background = '#adad1a'
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
 
-    } else if (hora >= 12 && hora < 18) {
-        //boa tarde
-        img.src = 'tarde.jpg'; 
-        document.body.style.background = '#446767'
+    if (ini.ariaValueMax.length == 0 || fim.ariaValueMax.length == 0 || passo.ariaValueMax.length == 0) {
+        window.alert('ERRO falta dados')
     } else {
-        //bom noite
-        img.src = 'noite.jpg'; 
-        document.body.style.background = '#2e2727'
+         res.innerHTML = 'Contando:'
+         let i = Number(ini.value)
+         let f = Number('fim.value')
+         let p = Number('passo.value')
+         
+         for(let c = i; c <= f; c += p ){
+            res.innerHTML +=  `${c}`
+         }
     }
+    
+
+
+
+
+
+
+
+
+
+
 }
